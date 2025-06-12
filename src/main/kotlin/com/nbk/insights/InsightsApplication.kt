dd4edf4528828c3561533e487af12f278b01157e
BarrakBM <barrak_mandani@live.com>
2025-06-12 20:19:19 +0300

    first commit

---
+++ src/main/kotlin/com/nbk/insights/InsightsApplication.kt
@@ -0,0 +1,11 @@
+package com.nbk.insights
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InsightsApplication
+
+fun main(args: Array<String>) {
+	runApplication<InsightsApplication>(*args)
+}
---
+++ src/test/kotlin/com/nbk/insights/InsightsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nbk.insights
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InsightsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
