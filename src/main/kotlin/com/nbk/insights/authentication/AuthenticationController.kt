f4d2de760ebe61280e277334d7420d9163763e1e
HAlGhanim <imzaji98@gmail.com>
2025-06-17 00:14:43 +0300

    Added getCurrentUser

--- src/main/kotlin/com/nbk/insights/authentication/AuthenticationController.kt
+++ src/main/kotlin/com/nbk/insights/authentication/AuthenticationController.kt
@@ -4,11 +4,13 @@ import com.nbk.insights.authentication.jwt.JwtService
 import com.nbk.insights.dto.authentication.LoginRequest
 import com.nbk.insights.dto.authentication.RegisterRequest
 import com.nbk.insights.repository.UserEntity
+import com.nbk.insights.repository.UserRepository
 import com.nbk.insights.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*
 class AuthController(
     private val authenticationManager: AuthenticationManager,
     private val userDetailsService: UserDetailsService,
+    private val userRepository: UserRepository,
     private val jwtService: JwtService,
     private val usersService: UserService,
     private val passwordEncoder: PasswordEncoder
@@ -51,4 +54,12 @@ class AuthController(
 
     @GetMapping("/users")
     fun listUsers() = mapOf("users" to usersService.getAll())
+
+    @GetMapping("/users/me")
+    fun getCurrentUser(): ResponseEntity<UserEntity> {
+        val username = SecurityContextHolder.getContext().authentication.name
+        val user = userRepository.findByUsername(username)
+            ?: throw UsernameNotFoundException("User not found with email: $username")
+        return ResponseEntity.ok(user)
+    }
 }
