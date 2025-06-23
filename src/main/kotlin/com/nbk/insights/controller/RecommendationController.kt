faa104cf0662a0ac6d5120b6776dbf2bf0023b58
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-06-23 18:13:11 +0300

    feat(Added new endpoint for offers) A new endpoint for offers have been added which gets offers by MCC category

--- src/main/kotlin/com/nbk/insights/controller/RecommendationController.kt
+++ src/main/kotlin/com/nbk/insights/controller/RecommendationController.kt
@@ -1,7 +1,10 @@
 package com.nbk.insights.controller
 
+import com.nbk.insights.dto.OfferDTO
 import com.nbk.insights.repository.UserRepository
+import com.nbk.insights.service.OffersService
 import com.nbk.insights.service.RecommendationsService
+import org.springframework.data.repository.query.Param
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UsernameNotFoundException
@@ -14,15 +17,25 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class RecommendationController(
     private val openAIService: RecommendationsService,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val offersService: OffersService
 ) {
 
-        @PostMapping("/ask")
-        fun ask(): ResponseEntity<String> {
-            val username = SecurityContextHolder.getContext().authentication.name
-            val userId = userRepository.findByUsername(username)?.id
-                ?: throw UsernameNotFoundException("User not found with username: $username")
-            val response = openAIService.getCategoryRecommendations(userId)
-            return ResponseEntity.ok(response)
-        }
+    @PostMapping("/ask")
+    fun ask(): ResponseEntity<String> {
+        val username = SecurityContextHolder.getContext().authentication.name
+        val userId = userRepository.findByUsername(username)?.id
+            ?: throw UsernameNotFoundException("User not found with username: $username")
+        val response = openAIService.getCategoryRecommendations(userId)
+        return ResponseEntity.ok(response)
+    }
+
+    @GetMapping("/offer/category")
+    fun getOffers(@RequestParam("category") category: String): ResponseEntity<List<OfferDTO>> {
+        val username = SecurityContextHolder.getContext().authentication.name
+        val userId = userRepository.findByUsername(username)?.id
+            ?: throw UsernameNotFoundException("User not found with username: $username")
+        val offers = offersService.getOffersByCategory(category)
+        return ResponseEntity.ok(offers)
+    }
 }
---
+++ src/main/kotlin/com/nbk/insights/dto/OffersDTO.kt
@@ -0,0 +1,6 @@
+package com.nbk.insights.dto
+
+data class OfferDTO(
+    val id: Long? = null,
+    val description: String
+)
