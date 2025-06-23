614c3a27485d149d43fe1d077f195d79d2cd1830
ahjadi <alihussainaljadi@gmail.com>
2025-06-23 11:29:36 +0300

    minor fix

--- src/main/kotlin/com/nbk/insights/service/TransactionsService.kt
+++ src/main/kotlin/com/nbk/insights/service/TransactionsService.kt
@@ -6,8 +6,6 @@ import com.nbk.insights.repository.AccountRepository
 import com.nbk.insights.repository.MccRepository
 import com.nbk.insights.repository.TransactionRepository
 import jakarta.persistence.EntityNotFoundException
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import com.hazelcast.logging.Logger
 import com.nbk.insights.helper.buildCacheKey
 import com.nbk.insights.serverInsightsCache
